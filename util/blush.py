358730520fbe2f2f95957770bf9f1715c9ef7e77
heesun22222 <sarah200221@gmail.com>
2024-07-13 01:19:16 +0900

    블러셔뀨

--- util/blush.py
+++ util/blush.py
@@ -2,7 +2,7 @@ import cv2
 import dlib
 import numpy as np
 from collections import OrderedDict
-
+#ELflflfllflflfflflflflfl
 # dlib 초기화
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
