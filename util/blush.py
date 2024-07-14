ba956b7ea0e53bf4be4211f7ae34e1ac463f4d29
heesun22222 <sarah200221@gmail.com>
2024-07-14 23:18:46 +0900

    utils.py생성

--- util/blush.py
+++ util/blush.py
@@ -2,25 +2,26 @@ import cv2
 import dlib
 import numpy as np
 import pandas as pd
-# 색상 정보를 JSON 파일에서 가져오는 함수 만들기. -> util.py로 따로 저장해야함.
-def get_color_from_json(prdCode):
-    # JSON 불러오기
-    json_path = 'products.json'
-    df = pd.read_json(json_path)
+from util.utils import get_color_from_json # util.py에서 get_color_from_json 함수를 import
+# # 색상 정보를 JSON 파일에서 가져오는 함수 만들기. -> util.py로 따로 저장해야함.
+# def get_color_from_json(prdCode):
+#     # JSON 불러오기
+#     json_path = 'products.json'
+#     df = pd.read_json(json_path)
 
-    # 색상정보, 옵션 1, 옵션2 가져오기
-    match_row = df[df['prdCode'] == prdCode]
-    if not match_row.empty:
-        info = match_row[['color', 'option1', 'option2']]
-        print(info)
+#     # 색상정보, 옵션 1, 옵션2 가져오기
+#     match_row = df[df['prdCode'] == prdCode]
+#     if not match_row.empty:
+#         info = match_row[['color', 'option1', 'option2']]
+#         print(info)
 
-        hex_color = info['color'].values[0].lstrip('#')
-        new_color = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
-    else:
-        print(f"No Matching prdCode: {prdCode}")
-        new_color = (0, 0, 0)
+#         hex_color = info['color'].values[0].lstrip('#')
+#         new_color = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+#     else:
+#         print(f"No Matching prdCode: {prdCode}")
+#         new_color = (0, 0, 0)
     
-    return (new_color[2], new_color[1], new_color[0])  # BGR로 변환
+#     return (new_color[2], new_color[1], new_color[0])  # BGR로 변환
 
 # dlib 초기화
 detector = dlib.get_frontal_face_detector()
