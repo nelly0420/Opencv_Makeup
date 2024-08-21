88e789101ded05c7b4eb2d485ab617fa6226038c
jung <130383085+w-astronuat@users.noreply.github.com>
2024-08-21 16:51:42 +0900

    lipstick matte 수정

--- util/lipstick.py
+++ util/lipstick.py
@@ -14,9 +14,10 @@ def gamma_correction(src: np.ndarray, gamma: float, coefficient: int = 1.2):
     return dst
 
 def apply_lipstick(image: np.ndarray, prdCode: str, color: str) -> np.ndarray:
-    # Convert color from hex to RGB and then to BGR
-    rgb_color = ImageColor.getrgb(color)
-    bgr_color = (rgb_color[2], rgb_color[1], rgb_color[0])  # Convert RGB to BGR
+    """주어진 색상과 옵션에 따라 립스틱 효과를 적용하는 함수"""
+    
+    usercolor = ImageColor.getrgb(color) #사용자 정의 color
+    user_bgr = (usercolor[2], usercolor[1], usercolor[0])  # Convert RGB to BGR
     
     # Extract the predefined lipstick color and other options from the JSON
     lip_color, option, _ = get_color_from_json(prdCode)
@@ -32,8 +33,8 @@ def apply_lipstick(image: np.ndarray, prdCode: str, color: str) -> np.ndarray:
     lip_points = get_lip_points(landmarks)
     
     # 사용자 정의 컬러가 있으면 립 색상 변경
-    if bgr_color != None:
-        lip_color = bgr_color
+    if user_bgr != None:
+        lip_color = user_bgr
 
     # Create masks
     mask = np.zeros_like(image)
@@ -42,65 +43,25 @@ def apply_lipstick(image: np.ndarray, prdCode: str, color: str) -> np.ndarray:
     contour_mask = np.zeros_like(image)
     contours, _ = cv2.findContours(cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(contour_mask, contours, -1, lip_color, 1) 
-    lip_liner = cv2.GaussianBlur(contour_mask, (19, 19), 0)
+    lip_liner = cv2.GaussianBlur(contour_mask, (19, 19), 1)
     
-    # image_with_lipstick = cv2.addWeighted(image, 1.0, mask, 0.4, 0.0)
-
-        # lips_only = np.zeros_like(image)
-        # #lips_only = cv2.fillPoly(lips_only, [lip_points], (180, 229, 255))
-        
-        # if option == "Glossy":
-        #     corrected_lips = gamma_correction(image_with_lipstick, gamma=1.2)
-        #     corrected_lips = cv2.GaussianBlur(corrected_lips, (5, 5), 3)
-        # elif option == "Matte":
-        #     #corrected_lips = gamma_correction(image_with_lipstick, gamma=1.5)
-        #     corrected_lips = cv2.GaussianBlur(lips_only, (5, 5), 0)
-            
-        # final_image = np.where(lips_only == np.array([255, 255, 255]), corrected_lips, image_with_lipstick)
-        
-        # (오리지날)
-        # if option == "Glossy":
-        #     mask = cv2.GaussianBlur(mask, (7, 7), 5)
-        #     image_with_lipstick = cv2.addWeighted(image, 1.0, mask, 0.4, 0.0)
-        #     corrected_lips = gamma_correction(image_with_lipstick, gamma=1.2)
-        #     corrected_lips = cv2.GaussianBlur(corrected_lips, (5, 5), 3)
-        
-        # (수정된소스 : 립스틱 윤각을 더 강조하고 블러효과를 좀 감소시킴.)
-        # if option == "Glossy":
-        #     # 마스크에 경계 강조 적용 (경계 검출)
-        #     edges = cv2.Canny(mask, 100, 200)  # Canny edge detector로 경계 강조
-        #     edges = cv2.dilate(edges, None)  # 경계 확대
-
-        #     # 경계 강조된 마스크를 원래 마스크와 결합
-        #     mask = cv2.bitwise_or(mask, edges)
-
-        #     # 이미지와 마스크 결합
-        #     image_with_lipstick = cv2.addWeighted(image, 1.0, mask, 0.5, 0.0)  # 가중치를 높임0.4 -> 0.5
-        #     corrected_lips = gamma_correction(image_with_lipstick, gamma=1.2)
-
-        # elif option == "Matte":
-        #     #print("mask dtype:", mask.dtype)
-        #     image_with_lipstick = cv2.addWeighted(image, 1.0, mask, 0.4, 0.0)
-        #     corrected_lips = gamma_correction(image_with_lipstick, gamma=1.5)
-        #     corrected_lips = cv2.addWeighted(corrected_lips, 1.5, corrected_lips, -0.5, 0)
-
+    corrected_lips = image_with_lipstick = np.copy(image)
 
     if option == "Glossy":
-        image_with_lipstick = cv2.addWeighted(image, 1.0, mask, 0.1, 0.0)
-        corrected_lips = gamma_correction(image_with_lipstick, gamma=1.2)
-        corrected_lips = cv2.GaussianBlur(corrected_lips, (15, 15), 1)
+        image_with_lipstick = cv2.addWeighted(image, 1.0, mask, 0.1, 0)
+        corrected_lips = gamma_correction(image_with_lipstick, 1.2)
+        corrected_lips = cv2.GaussianBlur(corrected_lips, (15, 15), 0)
         lip_liner = gamma_correction(lip_liner, 0.8, 1.5)
-        corrected_lips = cv2.addWeighted(corrected_lips, 0.8, lip_liner, 0.2, 0.0)
+        corrected_lips = cv2.addWeighted(corrected_lips, 0.8, lip_liner, 0.2, 0)
         corrected_lips = cv2.addWeighted(corrected_lips, 1.5, corrected_lips, -0.5, 0)  
     
-
     elif option == "Matte":
-        image_with_lipstick = cv2.addWeighted(image, 1.0, mask, 0.4, 0.0)
-        corrected_lips = gamma_correction(image_with_lipstick, gamma=1.5)
-        lip_liner = gamma_correction(lip_liner, 0.8, 1.5)
-        corrected_lips = cv2.addWeighted(corrected_lips, 0.7, lip_liner, 0.3, 0)  
+        image_with_lipstick = cv2.addWeighted(image, 1.0, mask, 0.3, 0)
+        corrected_lips = gamma_correction(image_with_lipstick, 1.5)
         corrected_lips = cv2.GaussianBlur(corrected_lips, (5, 5), 0)
-        corrected_lips = cv2.addWeighted(corrected_lips, 1.1, corrected_lips, -0.1, 0)  
+        #lip_liner = gamma_correction(lip_liner, 0.8, 1.0)
+        corrected_lips = cv2.addWeighted(corrected_lips, 0.7, lip_liner, 0.3, 0)  
+        corrected_lips = cv2.addWeighted(corrected_lips, 1.5, corrected_lips, -0.5, 0)  
 
     lips_only = np.zeros_like(image)
     lips_only = cv2.fillPoly(lips_only, [lip_points], (255, 255, 255))
